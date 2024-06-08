30c611e59d427bd1c751d885b6cebec08dc9978e
saqlain321 <saqlain8434@gmai.com>
2024-06-08 13:32:45 +0530

    bst2

---
+++ tree/bst2.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.key = key
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert_recursive(self.root, key)
+
+    def _insert_recursive(self, root, key):
+        if root is None:
+            return Node(key)
+        if key < root.key:
+            root.left = self._insert_recursive(root.left, key)
+        elif key > root.key:
+            root.right = self._insert_recursive(root.right, key)
+        return root
+
+    def search(self, key):
+        return self._search_recursive(self.root, key)
+
+    def _search_recursive(self, root, key):
+        if root is None or root.key == key:
+            return root
+        if key < root.key:
+            return self._search_recursive(root.left, key)
+        return self._search_recursive(root.right, key)
+
+    def inorder_traversal(self, root):
+        if root:
+            self.inorder_traversal(root.left)
+            print(root.key, end=" ")
+            self.inorder_traversal(root.right)
+
+# Example usage:
+bst = BinarySearchTree()
+bst.insert(50)
+bst.insert(30)
+bst.insert(20)
+bst.insert(40)
+bst.insert(70)
+bst.insert(60)
+bst.insert(80)
+
+print("Inorder traversal of the BST:")
+bst.inorder_traversal(bst.root)
+print("\nSearching for key 40:")
+result = bst.search(40)
+if result:
+    print("Key 40 found!")
+else:
+    print("Key 40 not found!")
